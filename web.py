9954dd0ffafcef73394334eb770a7c94146d4f77
wangdianwu <wangdianwu@126.com>
2020-11-27 18:34:47 +0800

    分模块

--- web.py
+++ web.py
@@ -5,8 +5,14 @@ from flask import Flask, redirect, url_for, request, Response, render_template, 
 from sql.sqlite3 import *
 import json
 import time
+
+#引入登陆模块
+from  login import  login_api
+
 app = Flask(__name__)
-app.secret_key = "jJInfdd4444dewp(f8e5ffkd*9&jfkl"      
+app.secret_key = "jJInfdd4444dewp(f8e5ffkd*9&jfkl"
+# 注册登录接口    
+app.register_blueprint(login_api)
 
 #修改开设课程
 @app.route('/updateCource', methods=['POST'])
@@ -49,44 +55,6 @@ def studentlist():
     result,_ = GetSql2("select * from t_student_info")
     return render_template("man_student_cap.html", results=result, result=r)
 
-#登录信息处理界面，处理由'/'传送过来的表单信息
-@app.route('/result', methods=['POST', 'GET'])   
-def result():
-    if request.method == 'POST':
-        #用户名
-        username = request.form.get('username') 
-        #密码
-        password = request.form.get('password')
-        #登陆类型
-        usertype = request.form.get('usertype')
-        name = ''
-        #学生
-        if usertype == 'student':
-            result, _ = GetSql2("select student_name from t_student_info where student_num = '"+username+"' and pwd = '"+password+"'")
-        #教师
-        if usertype == 'teacher':
-            result, _ = GetSql2("select teacher_name from t_teacher_info where teacher_num = '"+username+"' and pwd = '"+password+"'")
-        #管理员
-        if usertype == 'admin':
-            result, _ = GetSql2("select * from t_sys_info where sys_num = '"+username+"' and pwd = '"+password+"'")
-        #登录成功，页面跳转到相应的功能页面
-        if result: 
-            name = result[0][0]
-            if usertype == 'admin':
-                return render_template("manager_cap.html", name=username)
-            if usertype == 'teacher':
-                return render_template("teacher_cap.html", name=name, bianhao=username)
-            if usertype == 'student':
-                return render_template("student_cap.html", name=name, xuehao=username)
-        else:
-            # 没整明白
-            flash("用户名密码错误，请重新输入！")
-            # 密码错误重定向到登录页面
-            return redirect(url_for('index'))  
-
-    else:
-        # 重定向到登录页面
-        return redirect(url_for('index'))
 
 #新增教师
 @app.route('/add_teacher', methods=['POST', 'GET'])
@@ -349,11 +317,6 @@ def sel_cou():
         data = dict(student_num = xuehao, course_num = kehao, course_id = kexuhao)
         result = InsertData(data, "t_student_Course")
         return redirect(url_for('into_bixiu', xuehao=xuehao, result=result))
-
-@app.route('/')
-def index():
-    session.clear()
-    return render_template('login_cap.html')
 
 @app.route('/add_open', methods=['POST', 'GET'])
 def add_open():
